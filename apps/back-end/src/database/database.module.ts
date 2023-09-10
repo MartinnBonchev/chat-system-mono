26f5c352e20f617f9edac9f922d57c0c213753ef
Boyan Zhelev Extern <boyan.zhelev.extern@myrasecurity.com>
2023-09-10 16:19:52 +0300

    feat: add entities to db

--- apps/back-end/src/database/database.module.ts
+++ apps/back-end/src/database/database.module.ts
@@ -14,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         password: configService.getOrThrow('POSTGRES_PASSWORD'),
         autoLoadEntities: true,
         synchronize: true,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       }),
       inject: [ConfigService],
     }),
