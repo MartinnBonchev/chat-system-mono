a1419df9bb2d587dc215d79196316e997e8b205c
Boyan Zhelev Extern <boyan.zhelev.extern@myrasecurity.com>
2023-09-11 18:15:58 +0300

    feat: add service

--- apps/back-end/src/chat-room/chat-room.service.ts
+++ apps/back-end/src/chat-room/chat-room.service.ts
@@ -10,41 +10,101 @@ import { InjectRepository } from '@nestjs/typeorm';
 export class ChatRoomService {
   constructor(
     @InjectRepository(ChatRoom)
-    private ChatRoomRepo: Repository<ChatRoom>,
+    private chatRoomRepo: Repository<ChatRoom>,
     @InjectRepository(Message)
-    private MessageRepo: Repository<Message>,
+    private messageRepo: Repository<Message>,
     @InjectRepository(User)
-    private UserRepo: Repository<User>,
+    private userRepo: Repository<User>,
     @InjectRepository(UserChatRole)
-    private UserChatRoleRepo: Repository<UserChatRole>,
+    private userChatRoleRepo: Repository<UserChatRole>,
   ) {}
 
-  async createChatRoom(roomCreatorId: number, roomName: string) {
-    const room = this.ChatRoomRepo.create();
-    const role = this.UserChatRoleRepo.create();
-    const creator = await this.UserRepo.findOne({
+  async createChatRoom(roomCreatorId: string, roomName: string) {
+    const creator = await this.userRepo.findOneOrFail({
       where: { id: roomCreatorId },
     });
+    const room = this.chatRoomRepo.create({
+      name: roomName,
+      messages: [],
+      users: [creator],
+    });
+    const role = this.userChatRoleRepo.create({
+      role: 'admin',
+      user: creator,
+      chatRoom: room,
+    });
 
-    role.role = 'admin';
-    role.user = creator;
-    await this.UserChatRoleRepo.save(role);
+    await this.userChatRoleRepo.save(role);
 
-    room.name = roomName;
-    room.messages = [];
-    room.users = [creator];
-
-    return this.ChatRoomRepo.save(room);
+    return this.chatRoomRepo.save(room);
   }
-  async addToRoom() {}
-  async removeFromRoom() {}
-  async getChatRoom(id: number) {
+
+  async addToRoom(
+    roomId: string,
+    userId: string,
+    role: 'admin' | 'user' = 'user',
+  ) {
+    const room = await this.chatRoomRepo.findOneOrFail({
+      where: { id: roomId },
+    });
+    const user = await this.userRepo.findOneOrFail({ where: { id: userId } });
+
+    const userRole = this.userChatRoleRepo.create({
+      chatRoom: room,
+      role,
+      user,
+    });
+    await this.userChatRoleRepo.save(userRole);
+
+    room.users = [...room.users, user];
+
+    return this.chatRoomRepo.save(room);
+  }
+  async removeFromRoom(roomId: string, userId: string) {
+    const room = await this.chatRoomRepo.findOneOrFail({
+      where: { id: roomId },
+    });
+    const user = await this.userRepo.findOneOrFail({ where: { id: userId } });
+
+    const userRole = await this.userChatRoleRepo.findOneOrFail({
+      where: {
+        chatRoom: room,
+        user: user,
+      },
+    });
+
+    const userIndex = room.users.findIndex(({ id }) => id === user.id);
+    room.users = room.users.splice(userIndex, 1);
+
+    await this.userChatRoleRepo.delete(userRole);
+    return this.chatRoomRepo.save(room);
+  }
+  async getChatRoom(id: string) {
     try {
-      return this.ChatRoomRepo.findOneOrFail({ where: { id } });
+      return this.chatRoomRepo.findOneOrFail({ where: { id } });
     } catch (error) {
       throw new NotFoundException(`Chat room with id: ${id} does not exist`);
     }
   }
-  //   async sendMessage(chatId: number, senderId: number) {}
-  //   async getMessages(chatId: number) {}
+
+  async sendMessage(chatId: string, senderId: string, messageFromUser: string) {
+    const room = await this.chatRoomRepo.findOneOrFail({
+      where: { id: chatId },
+    });
+    const user = await this.chatRoomRepo.findOneOrFail({
+      where: { id: senderId },
+    });
+
+    const message = this.messageRepo.create({
+      chat_room: room,
+      user: user,
+      message: messageFromUser,
+    });
+
+    room.messages = [...room.messages, message];
+
+    await this.messageRepo.save(message);
+
+    return this.chatRoomRepo.save(room);
+  }
 }
